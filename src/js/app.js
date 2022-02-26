60a6991a35faaf4e0cbc28d5fc184cf58237d62f
aSpeeDAyad <dmitryiorzhev@gmail.com>
2022-02-26 08:52:02 +0200

    bad commit

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,5 @@ import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
 
-// core version + navigation, pagination modules:
-import Swiper, { Navigation, Pagination } from 'swiper';
-const swiper = new Swiper();
-
+const swiper = new Swiper('.swiper-slide');
+console.log('sssssssss');
